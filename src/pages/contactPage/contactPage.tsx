7ebc804ce527fb1c5a9b5032899ff745b1169ed5
vitoriaellen03 <contato.vitoriaellen03@gmail.com>
2024-12-08 19:28:23 -0300

    Ading for tailwindo for contact page

--- src/pages/contactPage/contactPage.tsx
+++ src/pages/contactPage/contactPage.tsx
@@ -1,60 +1,68 @@
 import React from "react";
+import './contactPage.css';
 
 const ContactPage = () => {
   return (
-    <section>
-      <section>
-        <h1>Get In Touch With Us</h1>
-        <p>
+    <section className="contact-page">
+      <section className="contact-header">
+        <h1 className="heading-primary">Get In Touch With Us</h1>
+        <p className="contact-description">
           For More Information About Our Product & Services, Please Feel Free To Drop Us
           An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!
         </p>
       </section>
-      
-      <section className="">
 
-        <div className="">
+      <section className="contact-info">
+        <div className="col contact-info-items">
           <div className="info-item">
-            <box-icon type='solid' name='map'></box-icon>
-            <h2>Address</h2>
-            <p>236 5th SE Avenue, New York NY10000, United States</p>
+            <box-icon type="solid" name="map" className="info-icon"></box-icon>
+            <div className="group">
+              <h2 className="info-title">Address</h2>
+              <p className="info-details">236 5th SE Avenue, New York NY10000, United States</p>
+            </div>
           </div>
 
           <div className="info-item">
-            <box-icon name='phone' type='solid' ></box-icon>
-            <h2>Phone</h2>
-            <p>
-              Mobile: (+84) 546-6789
-              <br />
-              Hotline: (+84) 456-6789
-            </p>
+            <box-icon name="phone" type="solid" className="info-icon"></box-icon>
+            <div className="group">
+              <h2 className="info-title">Phone</h2>
+              <p className="info-details">
+                Mobile: (+84) 546-6789
+                <br />
+                Hotline: (+84) 456-6789
+              </p>
+            </div>
           </div>
 
           <div className="info-item">
-            <box-icon type='solid' name='alarm'></box-icon>
-            <h2>Working Time</h2>
-            <p>
-              Monday-Friday: 9:00 - 22:00
-              <br />
-              Saturday-Sunday: 9:00 - 21:00
-            </p>
+            <box-icon type="solid" name="alarm" className="info-icon"></box-icon>
+            <div className="group">
+              <h2 className="info-title">Working Time</h2>
+              <p className="info-details">
+                Monday-Friday: 9:00 - 22:00
+                <br />
+                Saturday-Sunday: 9:00 - 21:00
+              </p>
+            </div>
           </div>
         </div>
 
-        <form className="">
-          <label htmlFor="name">Your name</label>
-          <input type="text" id="name" placeholder="Abc" />
+        <form className="col contact-form">
+          <div className="c-form">
+            <label htmlFor="name" className="form-label">Your name</label>
+            <input type="text" id="name" className="form-input" placeholder="Abc" />
 
-          <label htmlFor="email">Email address</label>
-          <input type="email" id="email" placeholder="Abc@def.com" />
+            <label htmlFor="email" className="form-label">Email address</label>
+            <input type="email" id="email" className="form-input" placeholder="Abc@def.com" />
 
-          <label htmlFor="subject">Subject</label>
-          <input type="text" id="subject" placeholder="This is an optional" />
+            <label htmlFor="subject" className="form-label">Subject</label>
+            <input type="text" id="subject" className="form-input" placeholder="This is optional" />
 
-          <label htmlFor="message">Message</label>
-          <textarea id="message" placeholder="Hi! I'd like to ask about"></textarea>
+            <label htmlFor="message" className="form-label">Message</label>
+            <textarea id="message" className="form-textarea" placeholder="Hi! I'd like to ask about"></textarea>
 
-          <button type="submit">Submit</button>
+            <button type="submit" className="form-submit-button">Submit</button>
+          </div>
         </form>
       </section>
     </section>
